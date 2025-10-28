9fc38ddb876df0fe96e2adf324185296372d51a3
Kerry N. Wood <kerry.wood@asterism.ai>
2025-10-28 06:00:23 -0600

    added comments

--- src/public_astrostandards_tools/utils.py
+++ src/public_astrostandards_tools/utils.py
@@ -17,6 +17,9 @@ def sv_to_osc( sv, PA ):
 
 #,  -----------------------------------------------------------------------------------------------------
 def sv_to_osc_df( sv_df : pd.DataFrame, PA ) :
+    ''' 
+    given a dataframe with 'teme_p' and 'teme_v' on each row, annotate each row with XA_KEP data
+    '''
     tv = sv_df.apply( lambda X: sv_to_osc(X,PA), axis=1 )
     tv = [ X.toDict() for X in tv ]
     tv = pd.DataFrame.from_dict( tv )
@@ -25,6 +28,9 @@ def sv_to_osc_df( sv_df : pd.DataFrame, PA ) :
 
 # -----------------------------------------------------------------------------------------------------
 def osc_to_mean( XA_KEP, PA ):
+    '''
+    take a XA_KEP structure and return a XA_KEP with *mean* fields
+    '''
     XA_KEP_MEAN = PA.helpers.astrostd_named_fields( PA.AstroFuncDll,  prefix='XA_KEP_' )
     PA.AstroFuncDll.KepOscToMean( XA_KEP.data, XA_KEP_MEAN.data )
     return XA_KEP_MEAN
